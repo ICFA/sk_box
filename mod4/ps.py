edc7330269c699700decd212e164eb17253aa92f
Айматова Шахзода <92733481+bloblobloblob@users.noreply.github.com>
2024-03-18 20:49:13 +0500

    Update ps.py

--- mod4/ps.py
+++ mod4/ps.py
@@ -1,19 +1,20 @@
 import shlex
+import string
 import subprocess
 from typing import List
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
-@app.route("/ps/", methods=["GET"])
+@app.route("/ps")
 def _ps():
-    args: List[str] = request.args.getlist("args", type=str)
-    flag = args[0]
-    command = shlex.split(f"ps {flag}")
+    args: list[str] = request.args.getlist('arg')
+    arguments_cleaned = [shlex.quote(s) for s in args]
+    command_str = f"ps {' '.join(arguments_cleaned)}"
+    command = shlex.split(command_str)
     result = subprocess.run(command, capture_output=True)
-    return f'<pre>{result.stdout.decode()}</pre>'
+    output = result.stdout.decode()
+    return string.Template("<pre>${output}</pre>").substitute(output=output)
 
-
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     app.run(debug=True)
